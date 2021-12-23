34c1019004090f4a7086d29180fd4f14fe603b9d
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2021-12-23 15:35:54 +0100

    Add file IO in warp request handler

--- Lek.hs
+++ Lek.hs
@@ -3,7 +3,14 @@ import Network.Wai
 import Network.HTTP.Types (status200)
 import Network.Wai.Handler.Warp (run)
 
-application _ respond = respond $
-  responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
+import System.Directory
+import Data.ByteString.Lazy
+import Data.String
 
-main = run 3000 application
+app :: Application
+app _request respond = do
+  paths <- listDirectory "Images"
+  let str = Prelude.concat paths
+  respond $ responseLBS status200 [("Content-Type", "text/plain")] $ fromString str
+
+main = run 3000 app
