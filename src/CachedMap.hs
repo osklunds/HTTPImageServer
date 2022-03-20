14de474ea69c2da66943a81d66c6df697d2b216c
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2022-03-20 11:22:29 +0100

    Add time expire to cached map

--- src/CachedMap.hs
+++ src/CachedMap.hs
@@ -4,25 +4,46 @@ module CachedMap
 ( CachedMap
 , new
 , get
+, getWithNow
+, maxAge
 )
 where
 
 import Data.Map hiding (map)
 import Prelude hiding (map, lookup)
+import Data.Time.Clock
 
 
-newtype CachedMap k v = CachedMap { map :: Map k v } deriving (Eq)
+newtype CachedMap k v = CachedMap { map :: Map k (UTCTime, v) } deriving (Eq)
 
 new :: CachedMap k v
 new = CachedMap { map = empty }
 
 get :: Ord k => k -> IO v -> CachedMap k v -> IO ((CachedMap k v), v)
-get key calcValue cachedMap@(CachedMap { map }) = do
+get key calcValue cachedMap = do
+    now <- getCurrentTime
+    getWithNow now key calcValue cachedMap
+
+getWithNow :: Ord k => UTCTime ->
+                       k ->
+                       IO v ->
+                       CachedMap k v ->
+                       IO ((CachedMap k v), v)
+getWithNow now key calcValue cachedMap@(CachedMap { map }) = do
     case lookup key map of
-        (Just value) ->
-            return (cachedMap, value)
+        (Just (timeOfCalc, value)) -> do
+            let diff = diffUTCTime now timeOfCalc
+            if diff >= maxAge
+                then do
+                    value <- calcValue
+                    let newMap = insert key (now, value) map
+                    return (CachedMap { map = newMap}, value)
+                else
+                    return (cachedMap, value)
         Nothing -> do
             value <- calcValue
-            let newMap = insert key value map
+            let newMap = insert key (now, value) map
             return (CachedMap { map = newMap}, value)
-            
+
+maxAge :: NominalDiffTime
+maxAge = 600
--- src/CachedMap/Tests.hs
+++ src/CachedMap/Tests.hs
@@ -7,6 +7,7 @@ import CachedMap
 
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
+import Data.Time.Clock
 
 prop_get :: Int -> Int -> Property
 prop_get key value = monadicIO $ do
@@ -35,8 +36,8 @@ prop_get_two_keys key1 key2 value1 value2 = key1 /= key2 ==> monadicIO $ do
     assert $ value2 == value2Gotten
     assert $ value1 == value1Gotten
 
-prop_get_get :: Int -> Int -> Int -> Property
-prop_get_get key value1 value2 = value1 /= value2 ==> monadicIO $ do
+prop_get_cached :: Int -> Int -> Int -> Property
+prop_get_cached key value1 value2 = value1 /= value2 ==> monadicIO $ do
     -- Arrange
     let map1 = new
     (map2, _value) <- run $ get key (return value1) map1
@@ -48,7 +49,20 @@ prop_get_get key value1 value2 = value1 /= value2 ==> monadicIO $ do
     assert $ map3 == map2
     assert $ value1 == valueGotten
 
+prop_get_cached_expired :: Int -> Int -> Int -> Property
+prop_get_cached_expired key value1 value2 = value1 /= value2 ==> monadicIO $ do
+    -- Arrange
+    let map1 = new
+    (map2, _value) <- run $ get key (return value1) map1
+    
+    -- Act
+    now <- run $ getCurrentTime
+    let newNow = addUTCTime (maxAge + 1) now
+    (map3, valueGotten) <- run $ getWithNow newNow key (return value2) map2
 
+    -- Assert
+    assert $ map3 /= map2
+    assert $ value2 == valueGotten
 
 return []
 runTests = $quickCheckAll
