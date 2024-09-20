970a945820b83f92027f40dbba35c26d7a4b64f9
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-09-20 10:57:12 +0200

    Helper function for checking string contents

--- src/Server/Tests.hs
+++ src/Server/Tests.hs
@@ -6,8 +6,12 @@ module Server.Tests where
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
 import Control.Concurrent
+import Control.Monad
 import Network.HTTP.Client
-import Data.ByteString.Lazy as LBS
+import Text.Regex.TDFA
+import Control.Exception as CE
+import Data.List
+import Data.ByteString.Lazy.Char8 as LBS8 (unpack)
 
 import Server
 
@@ -22,10 +26,35 @@ normalCases = do
 
     request <- parseRequest "http://127.0.0.1:12346"
     response <- httpLbs request manager
-
     killThread serverThread
 
-    LBS.putStr $ responseBody response
+    let resp = responseBody response
+    assertContainsStrings (LBS8.unpack resp)
+                          ["img2",
+                           "img3",
+                           "img3"]
+
+assertContainsStrings :: String -> [String] -> IO ()
+assertContainsStrings haystack needles = do
+    -- First check each individual string for easier debugging
+    forM_ needles (\needle -> if (needle `isInfixOf` haystack)
+                      then return ()
+                      else do
+                          let msg = "Could not find '" ++
+                                    needle ++
+                                    "' inside\n" ++
+                                    haystack
+                          throwIO (AssertionFailed msg))
+
+    -- Then check all at once to verify the order
+    let opts = defaultCompOpt{multiline = False}
+    let regex = makeRegexOpts opts
+                              defaultExecOpt
+                              (intercalate ".*" needles)
+
+    if match regex haystack
+       then return ()
+       else throwIO (AssertionFailed ("Regex not found in\n" ++ haystack))
 
 
 return []
