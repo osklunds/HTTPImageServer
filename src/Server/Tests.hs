839c9b7de0c802dd833e386d7d519785eee33ebf
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-09-21 08:32:14 +0200

    Create temp dir structure and test top level dirs HTML

--- src/Server/Tests.hs
+++ src/Server/Tests.hs
@@ -15,15 +15,38 @@ import Control.Exception as CE
 import Data.List
 import Data.ByteString.Lazy as LBS (putStr)
 import Data.ByteString.Lazy.Char8 as LBS8 
+import System.IO.Temp
+import System.FilePath
+import System.Directory
 
 import Server
 
 prop_normalCases :: Property
-prop_normalCases = monadicIO $ run $ normalCases
+prop_normalCases = monadicIO $ run $ do
+    withSystemTempDirectory "normalCases" (\thumbDir -> do
+        withSystemTempDirectory "normalCases" (\fullImageDir ->
+            normalCases thumbDir fullImageDir))
 
-normalCases :: IO ()
-normalCases = do
-    serverThread <- forkIO $ mainWithArgs "Test/Thumbs" "Test/Thumbs" 12346
+normalCases :: FilePath -> FilePath -> IO ()
+normalCases thumbDir fullImageDir = do
+    forM_ [thumbDir, fullImageDir]
+          (\dir -> do
+                       createDirectory $ dir </> "level1_1"
+                       createDirectory $ dir </> "level1_1" </> "level2_1"
+                       createDirectory $ dir </> "level1_1" </> "level2_2"
+                       createDirectory $ dir </> "level1_1" </> "level2_2" </>
+                                                 "level3"
+                       createDirectory $ dir </> "level1_1" </> "level2_2" </>
+                                                 "level3" </> "level4"
+                       createDirectory $ dir </> "level1_2"
+                       createDirectory $ dir </> "level1_3"
+                       -- TODO: Special chars
+          )
+
+    createDirectory $ thumbDir </> "onlyInThumbs"
+    createDirectory $ fullImageDir </> "onlyInFull"
+    
+    serverThread <- forkIO $ mainWithArgs thumbDir fullImageDir 12346
 
     manager <- newManager defaultManagerSettings
 
@@ -31,14 +54,36 @@ normalCases = do
     response <- httpLbs request manager
     killThread serverThread
 
-    -- LBS.putStr $ responseBody response
+    LBS.putStr $ responseBody response
 
     let resp = responseBody response
-    assertContainsStrings resp
-                          ["subfolder",
-                           "img2",
-                           "img3",
-                           "img3"]
+    assertContainsStrings resp [
+        -- Top button
+        "<div class=\"top_button\" height=\"30px\" >\n\
+        \/\n\
+        \\n\
+        \</div>",
+
+        -- Folder button 1
+        "<div class=\"folder_button\" onclick=\"window.location='/level1_1'\">\n\
+        \    /level1_1\n\
+        \</div>",
+
+        -- Folder button 2
+        "<div class=\"folder_button\" onclick=\"window.location='/level1_2'\">\n\
+        \    /level1_2\n\
+        \</div>",
+
+        -- Folder button 3
+        "<div class=\"folder_button\" onclick=\"window.location='/level1_3'\">\n\
+        \    /level1_3\n\
+        \</div>",
+
+        -- Folder button onlyInThumbs
+        "<div class=\"folder_button\" onclick=\"window.location='/onlyInThumbs'\">\n\
+        \    /onlyInThumbs\n\
+        \</div>"
+        ]
 
 assertContainsStrings :: ByteString -> [ByteString] -> IO ()
 assertContainsStrings haystack needles = do
