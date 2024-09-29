159c6b387b3af768e531240f2f748d2cf0f6f33d
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-09-29 12:05:09 +0200

    Test image pages with 1 and many image neighbors

--- src/Server/Tests.hs
+++ src/Server/Tests.hs
@@ -29,18 +29,28 @@ prop_normalCases = monadicIO $ run $ do
         withSystemTempDirectory "normalCases" (\fullImageDir ->
             normalCases thumbDir fullImageDir))
 
+-- This test is quite big, but it's a compromise between
+-- 1. Having an interesting file structure
+-- 2. Arranging and asserting names at the same scope
+-- 3. Having one test per concept
+-- In this test, 3 was sacrificed in favor of 1 and 2
 normalCases :: FilePath -> FilePath -> IO ()
 normalCases thumbDir fullImageDir = do
     -- Create directories and images
     forM_ [thumbDir, fullImageDir]
           (\dir -> do
                        createDirectory $ dir </> "level1_1"
-                       createDirectory $ dir </> "level1_1" </> "level2_1"
-                       createDirectory $ dir </> "level1_1" </> "level2_2"
-                       createDirectory $ dir </> "level1_1" </> "level2_2" </>
-                                                 "level3"
-                       createDirectory $ dir </> "level1_1" </> "level2_2" </>
-                                                 "level3" </> "level4"
+                       createDirectory $ dir </> "level1_1"
+                                             </> "level2_1"
+                       createDirectory $ dir </> "level1_1"
+                                             </> "level2_2"
+                       createDirectory $ dir </> "level1_1"
+                                             </> "level2_2"
+                                             </> "level3"
+                       createDirectory $ dir </> "level1_1"
+                                             </> "level2_2"
+                                             </> "level3"
+                                             </> "level4"
                        createDirectory $ dir </> "level1_2"
                        createDirectory $ dir </> "level1_3"
                        -- TODO: Special chars
@@ -53,12 +63,31 @@ normalCases thumbDir fullImageDir = do
     writeFile (thumbDir </> "level1_img2.jpg") "content_of_level1_img2_thumb"
     writeFile (fullImageDir </> "level1_img1.jpg") "content_of_level1_img1_full"
     writeFile (fullImageDir </> "level1_img2.jpg") "content_of_level1_img2_full"
+
+    writeFile (thumbDir </> "level1_1" </> "level11_img.jpg")
+              "content_of_level11_img_thumb"
+    writeFile (fullImageDir </> "level1_1" </> "level11_img.jpg")
+              "content_of_level11_img_full"
+
+    writeFile (thumbDir </> "level1_2" </> "level12_img1.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_img2.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_img3.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_img4.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_img5.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_img6.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_img7.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_img8.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_img9.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_imgA.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_imgB.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_imgC.jpg") ""
+    writeFile (thumbDir </> "level1_2" </> "level12_imgD.jpg") ""
     
     -- Start the server
     serverThread <- forkIO $ do
         -- TODO: Timeout is a hack to avoid port re-us issue. Find the proper
         -- Way to kill this thread if parent thread dies
-        timeout 1000000 $ mainWithArgs thumbDir fullImageDir 12345
+        timeout 5000000 $ mainWithArgs thumbDir fullImageDir 12345
         return ()
 
     -- Check responses folder pages
@@ -195,24 +224,218 @@ normalCases thumbDir fullImageDir = do
 
     assertResponseContainsStrings "/level1_img2.jpg.html" [
         -- The image itself
-        "background-image: url\\(\"/./level1_img2.jpg.full\"\\);",
+        "background-image",
+        "/./level1_img2.jpg.full",
 
         -- Navigation buttons
-        "<div class=\"left_button\" onclick=\"window.location='/./level1_img1.jpg.html';\"></div>",
-        "<div class=\"top_button\" onclick=\"window.location='/.';\"></div>",
+        "left_button",
+        "'/./level1_img1.jpg.html'",
+        "top_button",
+        "'/.'",
         "<div class=\"right_button\" ></div>",
 
         -- Preload of neighbor images
-        "<link rel=\"preload\" href=\"/./level1_img2.jpg.full\" as=\"image\"/>",
+        "preload",
+        "/./level1_img2.jpg.full",
+        "preload",
+        "/./level1_img2.jpg.html"
+        ]
 
-        -- Preload of neighbor pages
-        "<link rel=\"preload\" href=\"/./level1_img2.jpg.html\" as=\"image\"/>"
+    -- One image
+    assertResponseContainsStrings "/level1_1/level11_img.jpg.html" [
+        -- The image itself
+        "background-image",
+        "/level1_1/level11_img.jpg.full",
+
+        -- Navigation buttons
+        "<div class=\"left_button\" ></div>",
+        "top_button",
+        "'/level1_1'",
+        "<div class=\"right_button\" ></div>"
+        ]
+
+    -- Images so that all preloads are different
+    assertResponseContainsStrings "/level1_2/level12_img1.jpg.html" [
+        -- The image itself
+        "background-image",
+        "/level1_2/level12_img1.jpg.full",
+
+        -- Navigation buttons
+        "<div class=\"left_button\" ></div>",
+        "top_button",
+        "'/level1_2'",
+        "right_button",
+        "'/level1_2/level12_img2.jpg.html'",
+
+        -- Preload of neighbor images
+        "preload",
+        -- Backward
+        "/level1_2/level12_img1.jpg.full",
+        "/level1_2/level12_img1.jpg.full",
+        "/level1_2/level12_img1.jpg.full",
+        "/level1_2/level12_img1.jpg.full",
+        "/level1_2/level12_img1.jpg.full",
+        -- Current
+        "/level1_2/level12_img1.jpg.full",
+        -- Forward
+        "/level1_2/level12_img2.jpg.full",
+        "/level1_2/level12_img3.jpg.full",
+        "/level1_2/level12_img4.jpg.full",
+        "/level1_2/level12_img5.jpg.full",
+        "/level1_2/level12_img6.jpg.full",
+        -- Backward
+        "/level1_2/level12_img1.jpg.html",
+        "/level1_2/level12_img1.jpg.html",
+        "/level1_2/level12_img1.jpg.html",
+        "/level1_2/level12_img1.jpg.html",
+        "/level1_2/level12_img1.jpg.html",
+        -- Current
+        "/level1_2/level12_img1.jpg.html",
+        -- Forward
+        "/level1_2/level12_img2.jpg.html",
+        "/level1_2/level12_img3.jpg.html",
+        "/level1_2/level12_img4.jpg.html",
+        "/level1_2/level12_img5.jpg.html",
+        "/level1_2/level12_img6.jpg.html"
+        ]
+
+    assertResponseContainsStrings "/level1_2/level12_img2.jpg.html" [
+        -- The image itself
+        "background-image",
+        "/level1_2/level12_img2.jpg.full",
+
+        -- Navigation buttons
+        "left_button",
+        "'/level1_2/level12_img1.jpg.html'",
+        "top_button",
+        "'/level1_2'",
+        "right_button",
+        "'/level1_2/level12_img3.jpg.html'",
+
+        -- Preload of neighbor images
+        "preload",
+        -- Backward
+        "/level1_2/level12_img1.jpg.full",
+        "/level1_2/level12_img1.jpg.full",
+        "/level1_2/level12_img1.jpg.full",
+        "/level1_2/level12_img1.jpg.full",
+        "/level1_2/level12_img1.jpg.full",
+        -- Current
+        "/level1_2/level12_img2.jpg.full",
+        -- Forward
+        "/level1_2/level12_img3.jpg.full",
+        "/level1_2/level12_img4.jpg.full",
+        "/level1_2/level12_img5.jpg.full",
+        "/level1_2/level12_img6.jpg.full",
+        "/level1_2/level12_img7.jpg.full",
+        -- Backward
+        "/level1_2/level12_img1.jpg.html",
+        "/level1_2/level12_img1.jpg.html",
+        "/level1_2/level12_img1.jpg.html",
+        "/level1_2/level12_img1.jpg.html",
+        "/level1_2/level12_img1.jpg.html",
+        -- Current
+        "/level1_2/level12_img2.jpg.html",
+        -- Forward
+        "/level1_2/level12_img3.jpg.html",
+        "/level1_2/level12_img4.jpg.html",
+        "/level1_2/level12_img5.jpg.html",
+        "/level1_2/level12_img6.jpg.html",
+        "/level1_2/level12_img7.jpg.html"
+        ]
+
+    assertResponseContainsStrings "/level1_2/level12_img7.jpg.html" [
+        -- The image itself
+        "background-image",
+        "/level1_2/level12_img7.jpg.full",
+
+        -- Navigation buttons
+        "left_button",
+        "'/level1_2/level12_img6.jpg.html'",
+        "top_button",
+        "'/level1_2'",
+        "right_button",
+        "'/level1_2/level12_img8.jpg.html'",
+
+        -- Preload of neighbor images
+        "preload",
+        -- Backward
+        "/level1_2/level12_img2.jpg.full",
+        "/level1_2/level12_img3.jpg.full",
+        "/level1_2/level12_img4.jpg.full",
+        "/level1_2/level12_img5.jpg.full",
+        "/level1_2/level12_img6.jpg.full",
+        -- Current
+        "/level1_2/level12_img7.jpg.full",
+        -- Forward
+        "/level1_2/level12_img8.jpg.full",
+        "/level1_2/level12_img9.jpg.full",
+        "/level1_2/level12_imgA.jpg.full",
+        "/level1_2/level12_imgB.jpg.full",
+        "/level1_2/level12_imgC.jpg.full",
+        -- Backward
+        "/level1_2/level12_img2.jpg.html",
+        "/level1_2/level12_img3.jpg.html",
+        "/level1_2/level12_img4.jpg.html",
+        "/level1_2/level12_img5.jpg.html",
+        "/level1_2/level12_img6.jpg.html",
+        -- Current
+        "/level1_2/level12_img7.jpg.html",
+        -- Forward
+        "/level1_2/level12_img8.jpg.html",
+        "/level1_2/level12_img9.jpg.html",
+        "/level1_2/level12_imgA.jpg.html",
+        "/level1_2/level12_imgB.jpg.html",
+        "/level1_2/level12_imgC.jpg.html"
+        ]
+
+    assertResponseContainsStrings "/level1_2/level12_imgD.jpg.html" [
+        -- The image itself
+        "background-image",
+        "/level1_2/level12_imgD.jpg.full",
+
+        -- Navigation buttons
+        "left_button",
+        "'/level1_2/level12_imgC.jpg.html'",
+        "top_button",
+        "'/level1_2'",
+        "<div class=\"right_button\" ></div>",
+
+        -- Preload of neighbor images
+        "preload",
+        -- Backward
+        "/level1_2/level12_img8.jpg.full",
+        "/level1_2/level12_img9.jpg.full",
+        "/level1_2/level12_imgA.jpg.full",
+        "/level1_2/level12_imgB.jpg.full",
+        "/level1_2/level12_imgC.jpg.full",
+        -- Current
+        "/level1_2/level12_imgD.jpg.full",
+        -- Forward
+        "/level1_2/level12_imgD.jpg.full",
+        "/level1_2/level12_imgD.jpg.full",
+        "/level1_2/level12_imgD.jpg.full",
+        "/level1_2/level12_imgD.jpg.full",
+        "/level1_2/level12_imgD.jpg.full",
+        -- Backward
+        "/level1_2/level12_img8.jpg.html",
+        "/level1_2/level12_img9.jpg.html",
+        "/level1_2/level12_imgA.jpg.html",
+        "/level1_2/level12_imgB.jpg.html",
+        "/level1_2/level12_imgC.jpg.html",
+        -- Current
+        "/level1_2/level12_imgD.jpg.html",
+        -- Forward
+        "/level1_2/level12_imgD.jpg.html",
+        "/level1_2/level12_imgD.jpg.html",
+        "/level1_2/level12_imgD.jpg.html",
+        "/level1_2/level12_imgD.jpg.html",
+        "/level1_2/level12_imgD.jpg.html"
         ]
 
 
     -- TODO: Paths that don't exist
     -- TODO: Fail test if server crashes. If it returns, can kill parent thread
-    -- TODO: Enough images to test caching of neighbors
     
     -- Stop the server
     killThread serverThread
